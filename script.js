59c8d2c1c899247bd9268fce799d1e0aa571f249
Keshav Kumar <kumar2004keshav@gmail.com>
2026-04-23 11:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
-    //Write your code here
+	 const strong = document.querySelectorAll("strong");
+    boldWords.forEach(function(el) {
+        el.style.color = "rgb(0, 128, 0)";
+    });
+    
 
 
 }
 
 
 function return_normal() {
-    //Write your code here
+	
+const strong = document.querySelectorAll("strong");
+    boldWords.forEach(function(el) {
+        el.style.color = "rgb(0, 0, 0)";
+    });
 
     
 }
